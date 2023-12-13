cf59dd0a6ccf5ec2e52a7e6dcccbf3cddc9c8f22
Israel Malagutti <israelmalagutti@gmail.com>
2023-12-13 11:56:48 -0300

    Test - Debugging tests with render().debug()

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -3,7 +3,9 @@ import { render } from "@testing-library/react-native";
 import { Input } from "./";
 
 describe("Component: Input", () => {
-  it("should render without activity indicator", () => {
-    render(<Input />);
+  it("should render without ActivityIndicator", () => {
+    const { debug } = render(<Input />);
+
+    debug();
   });
 });
